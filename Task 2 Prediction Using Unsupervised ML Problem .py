15dc3b3fd3c82797042e0804abb57fc0638f3271
ShubhadaUsapkar <122629474+ShubhadaUsapkar@users.noreply.github.com>
2023-01-16 18:55:46 +0530

    Update Task 2 Prediction Using Unsupervised ML Problem .py

--- Task 2 Prediction Using Unsupervised ML Problem .py
+++ Task 2 Prediction Using Unsupervised ML Problem .py
@@ -1,64 +1,25 @@
-#!/usr/bin/env python
-# coding: utf-8
+
 
 # # The Sparks Foundation: Data Science and Business Analytics Intership
 # ## Prediction Using Unsupervised MLProblem 
 # ## Statement:From the given iris dataset predict the optimum number of clusters and represent it visually
 # ## Author: Shubhada Mangesh Usapkar
-# 
-
 # ## Step 1:Importing The Dataset
-
-# In[3]:
-
-
 # Step 1:Importing The Dataset
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.cluster import KMeans
-
-
-# In[4]:
-
-
 # Load the dataset
 data=pd.read_csv("C:/Users/Viraj Vijay Samant/Downloads/Iris (1).csv")
 data.head()      #To show the first five coulmns of data set
-
-
-# In[5]:
-
-
 data.drop("Id",axis=1,inplace=True)
-
-
 # ## Step2: Data Wrangling
-
-# In[6]:
-
-
 data.describe()
-
-
-# In[7]:
-
-
 data.info()
-
-
-# In[8]:
-
-
 data.Species.value_counts()
-
-
 # ## Step 3:Using the Elow Method to find the optimal number of clusters
-
-# In[9]:
-
-
 # Find the optimal number of clusters for k means clustering
 x=data.iloc[:,:-1].values
 from sklearn.cluster import KMeans
@@ -73,79 +34,29 @@ plt.title('The Elow Method')
 plt.xlabel('Number of clusters')
 plt.ylabel('WCSS')
 plt.show()
-
-
 # ### We choose the number of cluster as '3'
-# 
-
 # ## Step 4: Trainig the kmeans Model on the dataset
-# 
-
-# In[10]:
-
-
 # Apply kmeans to the dataset
 kmeans=KMeans(n_clusters=3,
             max_iter=300,n_init=10,random_state=0)
 y_kmeans=kmeans.fit_predict(x)
 y_kmeans
-
-
 # ## Step5: Visualize the test set result
-
-# In[11]:
-
-
 plt.scatter(x[y_kmeans==0,0], x[y_kmeans==0,1],
            s=100,c='red',label='Iris-setosa')
 plt.scatter(x[y_kmeans==1,0], x[y_kmeans==1,1],
            s=100,c='blue',label='Iris-versicolour')
 plt.scatter(x[y_kmeans==2,0], x[y_kmeans==2,1],
            s=100,c='green',label='Iris-virginica')
-
-
-# In[12]:
-
-
 # Plotting the centroids of the clusters
 plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1],
            s=100,c='yellow',label='Centroids')
 plt.legend()
-
-
 # ## Data Visualisation
-
-# In[13]:
-
-
 data.corr()
-
-
-# In[14]:
-
-
 plt.figure(figsize=(12,5))
 sns.heatmap(data.corr(),annot=True,cmap="BuPu")
-
-
-# In[15]:
-
-
 plt.figure(figsize=(8,6))
 sns.boxplot(x="Species",y="SepalLengthCm",data=data)
-
-
-# In[16]:
-
-
 sns.pairplot(data.corr())
-
-
 # ## Thank you!!!
-# 
-
-# In[ ]:
-
-
-
-
